ca87d997cc8b666a17dfbc29841f0bfc0c96bd04
DieAmeno <arminamusic@gmx.de>
2023-02-09 12:36:36 +0100

    first particle drawn + on click exlosion

--- src/ts/firework.ts
+++ src/ts/firework.ts
@@ -1,32 +1,57 @@
 namespace firework {
-    export let canvas: HTMLCanvasElement;
-    export let ctx: CanvasRenderingContext2D;
-  
-    // let rockets: Rocket[] = [];
-  
-    window.addEventListener("load", handlePageLoad);
-    window.addEventListener("resize", handleResize);
-  
-    function handlePageLoad(): void {
-      console.log("page loaded");
-  
-      //DOM Elements
-      canvas = document.getElementById("canvas") as HTMLCanvasElement;
-      ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
-  
-      // Set canvas width and height
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerWidth;
-      
-      const vector_test: Vector = new Vector(100, 100);
-      console.log(vector_test);
-      const particle_test: Particle = new Particle(vector_test);
-      particle_test.draw();
-    } 
-  
-    function handleResize(): void {
-      // Update canvas sizes
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-    }
+  export let canvas: HTMLCanvasElement;
+  export let ctx: CanvasRenderingContext2D;
+  let particles: Particle[] = [];
+  window.addEventListener("load", handlePageLoad);
+  window.addEventListener("resize", handleResize);
+
+  function handlePageLoad(): void {
+
+    //DOM Elements
+    canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+
+    // DOM Event Listener
+    canvas.addEventListener("click", handle_canvas_click);
+
+    // Set canvas width and height
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerWidth;
+    const vector_test: Vector = new Vector(100, 100);
+    particles.push(new Particle(vector_test, 122));
+    loop();
   }
+
+  function handle_canvas_click(e: MouseEvent): void {
+    console.log(e);
+
+    let particle_count = 1000;
+    let click_particles = [];
+    let click_hue = Utils.random_range(0, 360);
+
+    for (let i = 0; i <= particle_count; i++) {
+      click_particles.push(new Particle(new Vector(e.x, e.y), click_hue));
+    }
+
+    particles = [...particles, ...click_particles];
+  }
+
+  function handleResize(): void {
+    // Update canvas sizes
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+  function loop() {
+    requestAnimationFrame(loop);
+    ctx.globalCompositeOperation = "destination-out";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.globalCompositeOperation = "lighter";
+    particles.forEach((particle, i) => {
+      particle.animate();
+      if (!particle.is_visible()) {
+        particles.splice(i, 1);
+      }
+    });
+  }
+}
--- src/ts/particle.ts
+++ src/ts/particle.ts
@@ -1,32 +1,47 @@
 namespace firework {
-    export class Particle extends Moveable {
-      public last_positions: Vector[] = [];
-      public last_positions_max_count: number = 5;
-      public angle: number = Utils.random_range(0, Math.PI * 2);
-      public speed: number = Utils.random_range(1, 10);
-      public friction: number = 0.95;
-      public gravity: number = 1;
-      // Color hue
-      public hue: number;
-      //Color brightness
-      public brightness: number = Utils.random_range(60, 90);
-      public decay: number = Utils.random_range(0.05, 0.025);
-  
-      constructor(position: Vector) {
-        super(position);
+  export class Particle extends Moveable {
+    public last_positions: Vector[] = [];
+    public last_positions_max_count: number = 5;
+    public angle: number = Utils.random_range(0, Math.PI * 2);
+    public speed: number = Utils.random_range(1, 10);
+    public friction: number = 0.95;
+    public gravity: number = 1;
+    // Color hue
+    public hue: number;
+    //Color brightness
+    public brightness: number = Utils.random_range(60, 90);
+    public decay: number = Utils.random_range(0.05, 0.025);
+
+    constructor(position: Vector, hue: number) {
+      super(position);
+
+      for (let i = 0; i < this.last_positions_max_count; i++) {
+        this.last_positions.push(new Vector(position.x, position.y));
       }
-  
-      draw(): void {
-        console.log("drawing particle");
-        ctx.beginPath();
-        ctx.moveTo(this.position.x, this.position.y);
-        ctx.lineTo(this.position.x + 100, this.position.y + 100);
-        ctx.strokeStyle = "hsla(20, 95%, 88%, 0.8)";
-        ctx.stroke();
-      }
-  
-      update(): void {}
-  
-      animate(): void {}
+
+      this.hue = Utils.random_range(hue - 75, hue + 75);
     }
+
+    draw(): void {
+      ctx.beginPath();
+      let last_position = this.last_positions[this.last_positions.length - 1];
+      ctx.moveTo(last_position.x, last_position.y);
+      ctx.lineTo(this.position.x, this.position.y);
+      ctx.strokeStyle = `hsla(${this.hue}, 100%, ${this.brightness}%, ${this.alpha})`;
+      ctx.stroke();
+    }
+    update(): void {
+      this.position.x = this.position.x + Math.cos(this.angle) * this.speed;
+      this.position.y =
+        this.position.y + Math.sin(this.angle) * this.speed + this.gravity;
+      this.last_positions.pop();
+      this.last_positions.unshift(new Vector(this.position.x, this.position.y));
+      this.speed = this.speed * this.friction;
+      this.alpha = this.alpha - this.decay;
+    }
+    animate(): void {
+      this.update();
+      this.draw();
+    }
+  }
 }
